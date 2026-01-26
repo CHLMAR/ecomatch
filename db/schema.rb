760891e7e27891a76a10899091f32f8bce38b43e
Katarzyna Kruk <kasia.kruk99@gmail.com>
2026-01-26 16:33:12 +0100

    search model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_26_101640) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_26_150743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "searches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "uploaded_image"
+    t.string "uploaded_link"
+    t.string "system_prompt"
+    t.string "clothing_item"
+    t.string "clothing_material"
+    t.string "clothing_colour"
+    t.string "clothing_size"
+    t.string "clothing_brand"
+    t.float "clothing_price"
+    t.string "item_image"
+    t.string "item_name"
+    t.text "item_description"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +46,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_26_101640) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "searches", "users"
 end
