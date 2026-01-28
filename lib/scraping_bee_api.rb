bf8796a9d700c7e1dc64cb80bf5ec997d697a8e9
Katarzyna Kruk <kasia.kruk99@gmail.com>
2026-01-28 17:59:31 +0100

    scraping bee API is using premium proxy now, its fixed

--- lib/scraping_bee_api.rb
+++ lib/scraping_bee_api.rb
@@ -6,8 +6,8 @@ class ScrapingBeeApi
   def send_request(uploaded_link)
     search_url = URI.encode_www_form_component(uploaded_link)
     ai_query = URI.encode_www_form_component("return JSON: clothing_item, clothing_material, clothing_colour, clothing_brand, clothing_price (number), item_image (url), item_name, item_description")
-    #added js rendering
-    uri = URI("https://app.scrapingbee.com/api/v1?api_key=#{SCRAPINGBEE_API_KEY}&url=#{search_url}&render_js=true&ai_query=#{ai_query}")
+    # JS rendering + premium proxy for difficult e-commerce sites + wait for page load
+    uri = URI("https://app.scrapingbee.com/api/v1?api_key=#{SCRAPINGBEE_API_KEY}&url=#{search_url}&render_js=true&premium_proxy=true&wait=5000&ai_query=#{ai_query}")
 
     # Create client
     http = Net::HTTP.new(uri.host, uri.port)
@@ -24,7 +24,13 @@ class ScrapingBeeApi
     Rails.logger.info("ScrapingBee raw response: #{res.body}")
 
     if res.code == "200"
-      JSON.parse(res.body)
+      body = res.body.to_s.strip
+      if body.start_with?('{') || body.start_with?('[')
+        JSON.parse(body)
+      else
+        Rails.logger.error("ScrapingBee returned non-JSON response: #{body}")
+        nil
+      end
     else
       Rails.logger.error("ScrapingBee API error: #{res.code} - #{res.body}")
       nil
