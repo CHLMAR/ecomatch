87555c4b4af243b79c7c75016425ad1e3340bd6d
Jennifer Gossen <jennifer.gossen@web.de>
2026-01-30 10:58:00 +0100

    Restore wishlist on matches index (searches/:id/matches)
    - Controller: load @matches_by_product_id for search + comparison_products so each card has a match when one exists
    - Index view: pass match into match_card partial (match can be nil)
    - Match card: use saved_to_wishlist?(match) instead of product so wishlist checks match_id

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,13 +2,17 @@ class MatchesController < ApplicationController
   before_action :set_search
 
   def index
-    @comparison_products = ComparisonProduct.includes(:brand)
-    .by_clothing_item(params[:clothing_item])
-    .by_clothing_colour(params[:clothing_colour])
-    .by_clothing_material(params[:clothing_material])
-    .by_overall_rating(params[:overall_rating])
-
     @search = Search.find(params[:search_id])
+    @comparison_products = ComparisonProduct.includes(:brand)
+      .by_clothing_item(params[:clothing_item])
+      .by_clothing_colour(params[:clothing_colour])
+      .by_clothing_material(params[:clothing_material])
+      .by_overall_rating(params[:overall_rating])
+
+    product_ids = @comparison_products.pluck(:id)
+    @matches_by_product_id = @search.matches
+      .where(comparison_product_id: product_ids)
+      .index_by(&:comparison_product_id)
   end
 
   def show
