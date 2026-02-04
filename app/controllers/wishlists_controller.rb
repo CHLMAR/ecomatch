4798c47a86bd42586f7879db49ff8850d1576ccf
Charles Martin <charleslmartin4@gmail.com>
2026-02-04 11:42:03 +0100

    fixed wishilist redirects

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -3,6 +3,7 @@ class WishlistsController < ApplicationController
 
   def index
     @user = User.find(params[:id])
+    @search = Search.where(user: @user).last
     @wishlist_items = @user.wishlist_items.includes(comparison_product: :brand).order(created_at: :desc)
   end
 end
