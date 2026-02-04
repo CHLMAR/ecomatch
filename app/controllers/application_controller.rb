e3de7b2f1e8b8b83bd47ff0fb018f9210c0370d8
Charles Martin <charleslmartin4@gmail.com>
2026-02-04 15:56:59 +0100

    Working through checklist - meta tags added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def default_url_options
+    { host: ENV["HOST_NAME"] || "localhost:3000" }
+  end
 end
---
+++ config/initializers/default_meta.rb
@@ -0,0 +1,3 @@
+# Initialize default meta tags for the application using values from config/meta.yml
+
+DEFAULT_META = YAML.load_file(Rails.root.join(“config/meta.yml”))
